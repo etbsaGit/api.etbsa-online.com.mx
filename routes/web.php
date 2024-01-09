611347603ba95b38bff6d8dd12bdec363d99f6b1
AbrahamMZ <mga.abraham@gmail.com>
2024-01-09 16:30:50 -0600

    Add Enum

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Enums\AbsenceReason;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/enums/absence-reasons', function () {
+
+    $reasons = AbsenceReason::toArray();
+    $reason = AbsenceReason::fromReason('sick_leave')->getDisplayValue();
+    return compact('reasons','reason');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
@@ -20,35 +28,35 @@ Route::get('/', function () {
 
 Route::get('/clear-cache', function () {
     $exitCode = Artisan::call('cache:clear');
-    return '<h1>Cache facade value cleared </h1>'.$exitCode;
+    return '<h1>Cache facade value cleared </h1>' . $exitCode;
 });
 
 //Reoptimized class loader:
 Route::get('/optimize', function () {
     $exitCode = Artisan::call('optimize');
-    return '<h1>Reoptimized class loader</h1>'.$exitCode;
+    return '<h1>Reoptimized class loader</h1>' . $exitCode;
 });
 
 //Route cache:
 Route::get('/route-cache', function () {
     $exitCode = Artisan::call('route:cache');
-    return '<h1>Routes cached</h1>'.$exitCode;
+    return '<h1>Routes cached</h1>' . $exitCode;
 });
 
 //Clear Route cache:
 Route::get('/route-clear', function () {
     $exitCode = Artisan::call('route:clear');
-    return '<h1>Route cache cleared</h1>'.$exitCode;
+    return '<h1>Route cache cleared</h1>' . $exitCode;
 });
 
 //Clear View cache:
 Route::get('/view-clear', function () {
     $exitCode = Artisan::call('view:clear');
-    return '<h1>View cache cleared</h1>'.$exitCode;
+    return '<h1>View cache cleared</h1>' . $exitCode;
 });
 
 //Clear Config cache:
 Route::get('/config-cache', function () {
     $exitCode = Artisan::call('config:cache');
-    return '<h1>Clear Config cleared</h1>'.$exitCode;
+    return '<h1>Clear Config cleared</h1>' . $exitCode;
 });
