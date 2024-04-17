6c16c05b55c58b974d3456c7bba3ded960ef2501
AbrahamMZ <mga.abraham@gmail.com>
2024-04-17 16:40:37 -0600

    Base Repository Pattern

---
+++ app/Contracts/BaseContract.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Contracts;
+
+/**
+ * Interface BaseContract
+ * @package App\Contracts
+ */
+interface BaseContract
+{
+
+    /**
+     * @param array $params
+     * @param array $with
+     * @param callable|null $callable
+     * @return \Illuminate\Contracts\Pagination\LengthAwarePaginator
+     */
+    public function get(array $params = [], array $with = [], $callable = null);
+
+    /**
+     * Create a model instance
+     * @param array $attributes
+     * @return mixed
+     */
+    public function create(array $attributes);
+
+    /**
+     * Update a model instance
+     * @param array $attributes
+     * @param int $id
+     * @return mixed
+     */
+    public function update(array $attributes, int $id);
+
+    /**
+     * Return all model rows
+     * @param array $columns
+     * @param string $orderBy
+     * @param string $sortBy
+     * @return mixed
+     */
+    public function all($columns = array('*'), string $orderBy = 'id', string $sortBy = 'desc');
+
+    /**
+     * Find one by ID
+     * @param int $id
+     * @return mixed
+     */
+    public function find(int $id);
+
+    /**
+     * Find one by ID or throw exception
+     * @param int $id
+     * @return mixed
+     */
+    public function findOneOrFail(int $id);
+
+    /**
+     * Find based on a different column
+     * @param array $data
+     * @return mixed
+     */
+    public function findBy(array $data);
+
+    /**
+     * Find one based on a different column
+     * @param array $data
+     * @return mixed
+     */
+    public function findOneBy(array $data);
+
+    /**
+     * Find one based on a different column or through exception
+     * @param array $data
+     * @return mixed
+     */
+    public function findOneByOrFail(array $data);
+
+    /**
+     * Delete one by Id
+     * @param int $id
+     * @return mixed
+     */
+    public function delete(int $id);
+}
