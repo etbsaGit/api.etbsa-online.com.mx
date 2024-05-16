4820092c5f465f016ade0ffd8bfaeb035a6bd0d9
Cesar Aguayo <c.aguayo.s@outlook.com>
2024-05-16 11:42:19 -0600

    filter

--- app/Models/Ecommerce/Product.php
+++ app/Models/Ecommerce/Product.php
@@ -64,24 +64,30 @@ class Product extends Model
 
     public function scopeFilter($query, $filters)
     {
-        return $query
-            ->when(isset($filters['features']) && count($filters['features']) > 0, function ($query) use ($filters) {
-                $query->whereHas('features', function ($query) use ($filters) {
-                    foreach ($filters['features'] as $feature) {
-                        if (isset($feature['feature_id']) && isset($feature['value'])) {
-                            $query->where('feature_product.feature_id', $feature['feature_id'])
-                                ->where('feature_product.value', 'LIKE', '%' . $feature['value'] . '%');
-                        }
-                    }
-                }, '=', count($filters['features']));
-            })
-            ->when(isset($filters['categories']) && count($filters['categories']) > 0, function ($query) use ($filters) {
-                $query->whereHas('categories', function ($query) use ($filters) {
-                    $query->whereIn('categories.id', $filters['categories']);
-                }, '>=', count($filters['categories']));
-            })
-            ->when(isset($filters['brands']) && count($filters['brands']) > 0, function ($query) use ($filters) {
-                $query->whereIn('brand_id', $filters['brands']);
-            });
+        // Construimos la subconsulta para las características
+        if (isset($filters['features']) && count($filters['features']) > 0) {
+            foreach ($filters['features'] as $feature) {
+                if (isset($feature['feature_id']) && isset($feature['value'])) {
+                    $query->whereHas('features', function ($query) use ($feature) {
+                        $query->where('feature_product.feature_id', $feature['feature_id'])
+                            ->where('feature_product.value', $feature['value']);
+                    });
+                }
+            }
+        }
+
+        // Filtramos por categorías si se proporcionan
+        if (isset($filters['categories']) && count($filters['categories']) > 0) {
+            $query->whereHas('categories', function ($query) use ($filters) {
+                $query->whereIn('categories.id', $filters['categories']);
+            }, '>=', count($filters['categories']));
+        }
+
+        // Filtramos por marcas si se proporcionan
+        if (isset($filters['brands']) && count($filters['brands']) > 0) {
+            $query->whereIn('brand_id', $filters['brands']);
+        }
+
+        return $query;
     }
 }
